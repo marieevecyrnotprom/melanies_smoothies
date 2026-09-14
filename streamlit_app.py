ae94e23759f52ffd14f08b2e79768f5a2c2cdabb
marieevecyrnotprom <marie-eve.cyr@promutuel.ca>
2026-09-14 13:49:50 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,11 +14,11 @@ st.write('The name on your Smooties will be' , name_on_order)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
-
-
-ingredients_list = st.multiselect('Choose up to 5 ingredients:', my_dataframe , max_selections=5)
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
 st.stop()
+
+ingredients_list = st.multiselect('Choose up to 5 ingredients:', my_dataframe , max_selections=5)
+
 
 if ingredients_list:  
     ingredients_string = ''
